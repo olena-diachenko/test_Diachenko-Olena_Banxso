210aa3f7ea5aeba54a84cdd87ecf785da6ff19b5
Olena Diachenko <ellenka24@gmail.com>
2024-07-24 19:09:02 +0300

    fix: change swiper styles

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-
-const feedbacksSwiper = new Swiper('.swiper', {
-  direction: 'horizontal',
+const cardSwiper = new Swiper('.swiper', {
   slidesPerView: 3,
   spaceBetween: 48,
+  loop: true,
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
   navigation: {
     nextEl: '.swiper-button-next',
