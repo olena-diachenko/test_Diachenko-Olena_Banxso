20afe78d1d3a53c6ca264668215f529a975307e9
Olena Diachenko <ellenka24@gmail.com>
2024-07-24 18:04:55 +0300

    feature: implement layout

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+
+const feedbacksSwiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  slidesPerView: 3,
+  spaceBetween: 48,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
